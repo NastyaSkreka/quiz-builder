110c995ae2e87fddfc3f2137dd7b07e36f63c354
anastasiiaskreka <skreka.nastya@gmail.com>
2026-01-12 14:16:19 +0100

    feat: init

---
+++ backend/prisma/migrations/20260112125342_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Quiz" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "quizId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "prompt" TEXT NOT NULL,
+    "order" INTEGER NOT NULL,
+    "correctBoolean" BOOLEAN,
+    "correctText" TEXT,
+    CONSTRAINT "Question_quizId_fkey" FOREIGN KEY ("quizId") REFERENCES "Quiz" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Option" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "questionId" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "isCorrect" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "Option_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
